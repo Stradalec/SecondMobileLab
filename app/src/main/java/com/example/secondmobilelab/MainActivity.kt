7936394095aaf9d4cfe3adafde5a890408365e40
Stradalec <pochtatipo6@gmail.com>
2024-09-19 14:17:46 +0700

    Added Task 1

--- app/src/main/java/com/example/secondmobilelab/MainActivity.kt
+++ app/src/main/java/com/example/secondmobilelab/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.secondmobilelab
 
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,11 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-
+        val OurButton: Button = findViewById(R.id.btnToast);
+        OurButton.setOnClickListener {
+            val text = "Кнопка ОК"
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.setGravity(Gravity.CENTER, 0, 0)
+            toast.show()
         }
     }
 }
